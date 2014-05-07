1853a88221993a06edfc8ba2b5d98a6f14687947
Aleksei Zhakov <JustLexius@gmail.com>
2014-05-08 00:57:40 +0300

    Migration on Android Studio
    - working name changed to TicTacToe
    - restructured to suit Android Studio

---
+++ app/src/main/java/by/zhakov/tictactoe/activities/MainActivity.java
@@ -0,0 +1,28 @@
+package by.zhakov.tictactoe.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import by.zhakov.tictactoe.R;
+import by.zhakov.tictactoe.logic.MainController;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttons[][] = new Button[3][3];
+        buttons[0][0] = (Button) findViewById(R.id.btn_1_1);
+        buttons[0][1] = (Button) findViewById(R.id.btn_1_2);
+        buttons[0][2] = (Button) findViewById(R.id.btn_1_3);
+        buttons[1][0] = (Button) findViewById(R.id.btn_2_1);
+        buttons[1][1] = (Button) findViewById(R.id.btn_2_2);
+        buttons[1][2] = (Button) findViewById(R.id.btn_2_3);
+        buttons[2][0] = (Button) findViewById(R.id.btn_3_1);
+        buttons[2][1] = (Button) findViewById(R.id.btn_3_2);
+        buttons[2][2] = (Button) findViewById(R.id.btn_3_3);
+        MainController.start(buttons, this);
+    }
+    
+}
---
+++ app/src/main/java/by/zhakov/tictactoe/logic/FieldController.java
@@ -0,0 +1,68 @@
+package by.zhakov.tictactoe.logic;
+
+import android.widget.Button;
+import by.zhakov.tictactoe.model.Field;
+import by.zhakov.tictactoe.model.Player;
+
+public class FieldController{
+	private static final int winConditions[][] = {{11,12,13},{21,22,23},{31,32,33},{11,21,31},{12,22,32},{13,23,33},{11,22,33},{31,22,13}};
+	private static Field field = new Field();
+	private static ButtonController buttonControllers[][] = new ButtonController[3][3];
+
+	public static void start(Button[][] buttons){
+		for (byte i = 0; i < 3; i++){
+			for (byte q = 0; q < 3; q++){
+				buttonControllers[i][q] = new ButtonController(i, q, buttons[i][q]);
+			}
+		}
+	}
+	
+	public static void changeCell(int x, int y){
+		Player player = MainController.getPlayer();
+		field.setCell(x, y, player.getSymbol());
+		buttonControllers[x][y].update(player);
+		if (checkVictory(player)){
+			MainController.victory(player);
+		}
+		if (checkDraw()){
+			MainController.draw();
+		}
+	}
+	
+	private static boolean checkVictory(Player player){
+		boolean victory = false;
+		for (int i = 0; i < winConditions.length; i++){
+			int x1 = (winConditions[i][0] % 10) - 1;
+			int y1 = (winConditions[i][0] / 10) - 1;
+			int x2 = (winConditions[i][1] % 10) - 1;
+			int y2 = (winConditions[i][1] / 10) - 1;
+			int x3 = (winConditions[i][2] % 10) - 1;
+			int y3 = (winConditions[i][2] / 10) - 1;
+			if ((field.getCell(x1, y1) == field.getCell(x2, y2)) && (field.getCell(x2, y2) == field.getCell(x3, y3)) &&
+					 (field.getCell(x1, y1) == player.getSymbol())){
+				victory = true;
+			}
+		}
+		return victory;
+	}
+	
+	private static boolean checkDraw(){
+		for (int i = 0; i < 3; i++){
+			for (int q = 0; q < 3; q++){
+				if (field.getCell(i, q) == ' '){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static void cleanField(){
+		for (byte i = 0; i < 3; i++){
+			for (byte q = 0; q < 3; q++){
+				buttonControllers[i][q].update(null);
+			}
+		}
+		field = new Field();
+	}
+}
